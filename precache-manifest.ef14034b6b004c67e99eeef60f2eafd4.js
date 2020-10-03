3a0cd3defe31a80193646d7152a528b0357a1ea9
Jesus Seiler <ichbinseiler@gmail.com>
2020-10-03 17:08:53 -0300

    Updates

---
+++ precache-manifest.ef14034b6b004c67e99eeef60f2eafd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41ef4ecd399642a8d71",
+    "url": "/RayenApp/static/css/main.3298fdc9.chunk.css"
+  },
+  {
+    "revision": "c41ef4ecd399642a8d71",
+    "url": "/RayenApp/static/js/main.f30a6e7b.chunk.js"
+  },
+  {
+    "revision": "83c50c1397a8a35e5978",
+    "url": "/RayenApp/static/js/runtime~main.471c5c4b.js"
+  },
+  {
+    "revision": "d159d357378f7aff4c8f",
+    "url": "/RayenApp/static/js/2.f110dc6e.chunk.js"
+  },
+  {
+    "revision": "af5ef0fe31382d3b919e0272ac612cb5",
+    "url": "/RayenApp/index.html"
+  }
+];
